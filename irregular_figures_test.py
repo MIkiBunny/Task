d7a9b8fa2a899a600b34c199855feb534d855cc8
MIkiBunny <85002396+MIkiBunny@users.noreply.github.com>
2024-06-05 14:54:11 +0300

    test

--- irregular_figures_test.py
+++ irregular_figures_test.py
@@ -4,9 +4,12 @@ import irregular_figures
 
 
 @pytest.mark.parametrize('side, coorx, coory, expected', [
+    #Circle
+    (0, [0, 0], [0, 3], (28.27, 18.85)),
+    #(0, ['n', 0], [0, 2], 'TypeError'), (not work)
+    #polygon
     (3, [0, 3, 0], [0, 0, 4], (6.0, 12)),
-    (4, [0, 4, 4, 0], [0, 0, 4, 4], (16.0, 16.0)),
-    (0, [0, 0], [0, 3], (28.27, 18.85))
+    (4, [0, 4, 4, 0], [0, 0, 4, 4], (16.0, 16.0))
 ])
 def test_irregular_figures(side, coorx, coory, expected):
     if side == 0:
